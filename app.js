01f7842ebe6e59176b12fdf9b8404aeea61bce9b
Olivia <oliviatmanalastas@gmail.com>
2023-01-17 13:21:28 -0500

    require without creating a variable

--- app.js
+++ app.js
@@ -2,7 +2,11 @@
 // Modules - Encapsulated code
 const names = require('./4-firstModule')
 const sayHi = require('./5-utils')
+const data = require('./6-alt-flavor')
+require('./7-mind-grenade')
 
+console.log(data)
 console.log(names)
+
 sayHi(names.john)
 sayHi(names.peter)
