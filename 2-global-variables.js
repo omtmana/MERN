33b22503448ad438a7fc0f16fd7c47e1b1a2133b
Olivia <oliviatmanalastas@gmail.com>
2023-01-17 12:57:16 -0500

    Node modules

--- 2-global-variables.js
+++ 2-global-variables.js
@@ -5,3 +5,8 @@
 // require     - function to use modules(CommonJS)
 // module      - info about current module (file)
 // process     - info about env where the program is being executed
+
+console.log(__dirname)
+setInterval(() => {
+   console.log('Hello World')
+}, 1000)
---
+++ app.js
@@ -0,0 +1,12 @@
+// Modules
+
+const john = 'john'
+const peter = 'peter'
+
+const sayHi = (name) => {
+   console.log(`Hello ${name}`)
+}
+
+sayHi('Susan')
+sayHi(john)
+sayHi(peter)
